842964334fceca6e5b232ddca8ab8f41ba0bd276
heyyakash <akashsharma2002@gmail.com>
2025-02-01 22:57:39 +0530

    initialized backend

---
+++ backend/src/main/java/com/mnemos/backend/Entity/Users.java
@@ -0,0 +1,30 @@
+package com.mnemos.backend.Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
